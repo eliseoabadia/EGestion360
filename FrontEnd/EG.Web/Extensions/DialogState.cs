79d44b1614325a513f5ff07a405ef7400d6c0668
eliseoabadia <eliseo.abadia@gmail.com>
2026-02-21 18:54:03 -0600

    Etapa dos para mejorar el código

---
+++ FrontEnd/EG.Web/Extensions/DialogState.cs
@@ -0,0 +1,20 @@
+﻿namespace EG.Web.Extensions;
+public class DialogState
+{
+    private Dictionary<string, object> _forms = new();
+
+    public void RegisterForm(string key, object form)
+    {
+        _forms[key] = form;
+    }
+
+    public T? GetForm<T>(string key) where T : class
+    {
+        return _forms.TryGetValue(key, out var form) ? form as T : null;
+    }
+
+    public void Clear()
+    {
+        _forms.Clear();
+    }
+}
